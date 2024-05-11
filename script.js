89131752b292303c52afef31f7a66829be8871dc
Kamronbek777 <kamronbek25082010@gmail.com>
2024-05-11 21:57:27 +0500

    Math

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("There will generate a random numbers");
+let randomNumber = Math.round(Math.random() * 99);
+alert(`Randomly generated nuber is ${randomNumber}`);
+
+confirm("Now we will multiply it by 2 and then divided it by 2 ");
+alert(`The random number was ${randomNumber} We multiplied it by 2 and got ${randomNumber * 2} and we divided it by 2 and got ${randomNumber / 2} and the remainder will be ${randomNumber / 2 % 3}`);
